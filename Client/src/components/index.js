6ff5ab18bbd25249aa0070c761f5b0d88c73ecb5
Gaj Singh Rathore <gajrathore353@gmail.com>
2025-01-24 20:53:25 +0530

    fixed typo error

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -1,12 +1,12 @@
 import { Login } from "./Login";
 import { AuthLayout } from "./AuthLayout";
 import { Input } from "./Input";
-import { SignUp } from "./Signup";
 import { Header } from "./Header";
+import {Signup} from "./Signup"
 import { TaskList } from "./TaskList";
 import { CreateTaskForm } from "./CreateTaskForm";
 import { TaskFilter } from "./TaskFilter";
 
 
 
-export {Login,AuthLayout,Input,SignUp,Header,TaskList,CreateTaskForm,TaskFilter}
+export {Login,AuthLayout,Input,Signup,Header,TaskList,CreateTaskForm,TaskFilter}
